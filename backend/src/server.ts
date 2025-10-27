71e7674dd936490b7a03f913748dca194f517135
Elham Bashari <elhambashari84@gmail.com>
2025-10-27 11:56:46 +0100

    GET/api/users created and 200 ok in insomnia

---
+++ backend/src/server.ts
@@ -0,0 +1,32 @@
+
+import express from "express";
+     import users from "./routes/users.js";
+     import channels from "./routes/channels.js";
+     import messages from "./routes/messages.js";
+	
+
+
+
+
+     const app = express();
+
+     app.use(express.json());
+
+     
+     app.use("/api/users", users);
+     console.log("✅ Users route loaded"); 
+     app.use("/api/channels", channels);
+     app.use("/api/messages", messages);
+	
+
+     
+     app.use((req, res, next) => {
+       console.log(`📡 Received request: ${req.method} ${req.url}`);
+       next();
+     });
+
+     const PORT = process.env.PORT || 5000;
+     console.log("🔄 Server starting...");
+     app.listen(PORT, () => {
+       console.log(`✅ Server is running on http://localhost:${PORT}`);
+     });
